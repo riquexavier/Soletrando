d989c9948c26dd6247bfe9992086933fac4bfaf7
Pedrox | Adm! Dev! <pedrooohhenriquex@hotmail.com>
2024-03-31 13:04:01 -0300

    Soletrando

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine();
+
+Console.WriteLine("Digite o texto que deseja ser soletrado:");
+
+Console.WriteLine();
+
+string txt = Console.ReadLine()!;
+
+Console.WriteLine("\nSoletrando fica:\n");
+
+Console.WriteLine(string.Join("-",txt.ToUpper().ToCharArray()));
+
+Console.WriteLine();
